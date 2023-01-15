fab2abd1f127de719a835e97ea210abd340c66ac
Arnapurna Bedi <bediarnapurna20@gmail.com>
2023-01-15 21:40:46 +0530

    js file added

---
+++ 07-Blurry Loading/script.js
@@ -0,0 +1,20 @@
+const bg = document.querySelector('.bg')
+const loadText = document.querySelector('.loading-text')
+
+let load = 0
+let int = setInterval(blurring, 30);
+
+function blurring() {
+    load++;
+    if (load > 99) {
+        clearInterval(int);
+    }
+    // console.log(load);
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+// map a range of numbers to another range of numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
